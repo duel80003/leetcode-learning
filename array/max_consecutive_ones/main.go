925960c4f41df333b34ec839fffddffb1971376b
duel80003 <duel80003@gmail.com>
2021-02-13 15:20:32 +0800

    new problem

---
+++ array/max_consecutive_ones/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func findMaxConsecutiveOnes(nums []int) int {
+	max, cur := 0, 0
+	for _, v := range nums {
+		if v == 1 {
+			cur++
+		} else {
+			if max == 0 || cur > max {
+				max = cur
+			}
+			cur = 0
+		}
+	}
+	if cur > max {
+		return cur
+	}
+	return max
+}
+
+func main() {
+	a1 := []int{1, 0, 1, 1, 0, 1}
+	fmt.Println(findMaxConsecutiveOnes(a1))
+	a2 := []int{1, 0, 1, 1}
+	fmt.Println(findMaxConsecutiveOnes(a2))
+}
